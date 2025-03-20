76ca94270db7b71ee817399e87d554db19bdf0cd
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-20 19:12:55 -0300

    implementation delete, insert and getAll methods for SpecialtyRepository

--- src/Infrastructure/Repositories/SpecialtyRepository.cs
+++ src/Infrastructure/Repositories/SpecialtyRepository.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using ClinAgenda.Application.DTOs;
 using Dapper;
 using MySql.Data.MySqlClient;
@@ -25,6 +26,53 @@ namespace ClinAgenda.Infrastructure.Repositories
             var specialty = await _connection.QueryFirstOrDefaultAsync<SpecialtyDTO>(query, new { Id = id });
 
             return specialty;
+        }
+
+        public async Task<int> DeleteSpecialtyAsync(int id)
+        {
+            string query = @"
+            DELETE FROM SPECIALTY
+            WHERE ID = @Id";
+
+            var parameters = new { Id = id };
+
+            var rowsAffected = await _connection.ExecuteAsync(query, parameters);
+
+            return rowsAffected;
+        }
+
+        public async Task<int> InsertSpecialtyAsync(SpecialtyInsertDTO specialtyInsertDTO)
+        {
+            string query = @"
+            INSERT INTO SPECIALTY (NAME, SCHEDULEDURATION) 
+            VALUES (@Name, @Scheduleduration);
+            SELECT LAST_INSERT_ID();";
+            return await _connection.ExecuteScalarAsync<int>(query, specialtyInsertDTO);
+        }
+
+        public async Task<(int total, IEnumerable<SpecialtyDTO> specialtys)> GetAllAsync(int? itemsPerPage, int? page)
+        {
+            var queryBase = new StringBuilder(@"
+                FROM SPECIALTY S WHERE 1 = 1");
+
+            var parameters = new DynamicParameters();
+
+            var countQuery = $"SELECT COUNT(DISTINCT S.ID) {queryBase}";
+            int total = await _connection.ExecuteScalarAsync<int>(countQuery, parameters);
+
+            var dataQuery = $@"
+            SELECT ID, 
+            NAME, 
+            SCHEDULEDURATION
+            {queryBase}
+            LIMIT @Limit OFFSET @Offset";
+
+            parameters.Add("Limit", itemsPerPage);
+            parameters.Add("Offset", (page - 1) * itemsPerPage);
+
+            var specialtys = await _connection.QueryAsync<SpecialtyDTO>(dataQuery, parameters);
+
+            return (total, specialtys);
         }
     }
 }
