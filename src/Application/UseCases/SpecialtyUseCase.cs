ead1ca9d0a24fb873f16f58b2cfa899dc18daf43
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-20 19:43:51 -0300

    add service layer for specialty - SpecialtyUseCase

---
+++ src/Application/UseCases/SpecialtyUseCase.cs
@@ -0,0 +1,39 @@
+using ClinAgenda.Application.DTOs;
+using ClinAgenda.Core.Interfaces;
+
+namespace ClinAgenda.Application.UseCases
+{
+    public class SpecialtyUseCase
+    {
+        private readonly ISpecialtyRepository _specialtyRepository;
+
+        public SpecialtyUseCase(ISpecialtyRepository specialtyRepository)
+        {
+            _specialtyRepository = specialtyRepository;
+        }
+
+        public async Task<object> GetSpecialtyAsync(int itemsPerPage, int page)
+        {
+            var (total, rawData) = await _specialtyRepository.GetAllAsync(itemsPerPage, page);
+
+            return new
+            {
+                total,
+                items = rawData.ToList()
+            };
+        }
+
+        public async Task<int> CreateSpecialtyAsync(SpecialtyInsertDTO specialtyDTO)
+        {
+           
+            var newSpecialtyId = await _specialtyRepository.InsertSpecialtyAsync(specialtyDTO);
+
+            return newSpecialtyId;
+
+        }
+        public async Task<SpecialtyDTO?> GetSpecialtyByIdAsync(int id)
+        {
+            return await _specialtyRepository.GetByIdAsync(id);
+        }
+    }
+}
