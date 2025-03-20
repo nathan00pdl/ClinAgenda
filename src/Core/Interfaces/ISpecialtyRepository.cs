aeb2ebf6693a5b30fc4feb7ca8b57d712a7f33bd
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-20 19:16:30 -0300

    refactoring interface ISpecialtyRepository

--- src/Core/Interfaces/ISpecialtyRepository.cs
+++ src/Core/Interfaces/ISpecialtyRepository.cs
@@ -4,9 +4,9 @@ namespace ClinAgenda.Core.Interfaces
 {
     public interface ISpecialtyRepository
     {
-        Task<SpecialtyDTO> GetByIdAsync(int Id);
-        Task<int> DeleteStatusAsync(int id);
-        Task<int> InsertStatusAsync(StatusInsertDTO statusInsertDTO);
+        Task<SpecialtyDTO> GetByIdAsync(int id);
+        Task<int> DeleteSpecialtyAsync(int id);
+        Task<int> InsertSpecialtyAsync(SpecialtyInsertDTO specialtyInsertDTO);
         Task<(int total, IEnumerable<SpecialtyDTO> specialtys)> GetAllAsync(int? itemsPerPage, int? page);
     }
 }
