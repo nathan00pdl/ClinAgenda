4e7832a61d3ab19035bfca911b6ff3e71df82abc
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-20 19:08:48 -0300

    add attribute in SpecialtyInsertDTO

--- src/Application/DTOs/Specialty/SpecialtyInsertDTO.cs
+++ src/Application/DTOs/Specialty/SpecialtyInsertDTO.cs
@@ -3,5 +3,6 @@ namespace ClinAgenda.Application.DTOs
     public class SpecialtyInsertDTO
     {
         public required String Name { get; set; }
+        public int ScheduleDuration { get; set; }
     }
 }
