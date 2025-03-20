0513d3edd4485a47dd90d66879d7b9c7dcd5bdca
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-20 13:35:38 -0300

    implementation of  StatusController and List method as HTTP GET request

---
+++ src/WebAPI/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+using ClinAgenda.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinAgenda.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/status")]
+    public class StatusController : ControllerBase
+    {
+        private readonly StatusUseCase _statusUseCase;
+
+        public StatusController(StatusUseCase service)
+        {
+            _statusUseCase = service;
+        }
+
+        [HttpGet("List")]
+        public async Task<IActionResult> GetStatusAsync([FromQuery] int itemsPerPage = 10, [FromQuery] int page = 1)
+        {
+            try
+            {
+                var specialty = await _statusUseCase.GetStatusAsync(itemsPerPage, page);
+                return Ok(specialty);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error When Fetching Status: {ex.Message}");
+            }
+        }
+    }
+}
