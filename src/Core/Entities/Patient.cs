588cc11ab37e8554b743083055f7369ba9468b00
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-21 11:02:24 -0300

    changes to the attributes of the patient entity

--- src/Core/Entities/Patient.cs
+++ src/Core/Entities/Patient.cs
@@ -5,9 +5,10 @@ namespace ClinAgenda.Core.Entities
     public class Patient
     {
         public int Id { get; set; }
-        public required string name { get; set; }
-        public required string PhoneNumber { get; set; }
-        public required string DocumentNumber { get; set; }
+        public required String name { get; set; }
+        public required String PhoneNumber { get; set; }
+        public required String DocumentNumber { get; set; }
         public required int StatusId { get; set; }
+        public required String BirthDate { get; set; }
     }
 }
