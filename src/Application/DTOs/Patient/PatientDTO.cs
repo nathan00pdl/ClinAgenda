3571033f5f2ab22a82ed5ec695752149515ebc97
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-21 11:05:08 -0300

    add PatientDTO

--- src/Application/DTOs/Patient/PatientDTO.cs
+++ src/Application/DTOs/Patient/PatientDTO.cs
@@ -1,14 +1,14 @@
-using System.Runtime.CompilerServices;
+using ClinAgenda.Core.Entities;
 
-namespace ClinAgenda.Core.Entities
+namespace ClinAgenda.Application.DTOs.Patient
 {
-    public class Patient
+    public class PatientDTO
     {
         public int Id { get; set; }
         public required String Name { get; set; }
         public required String PhoneNumber { get; set; }
         public required String DocumentNumber { get; set; }
-        public required int StatusId { get; set; }
+        public required StatusDTO Status { get; set; }
         public required String BirthDate { get; set; }
     }
 }
