0c869a8ddc256f5b785420c9eaea563fe3ff84a6
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-25 14:20:47 -0300

    study comment

--- src/Application/DTOs/Status/StatusInsertDTO.cs
+++ src/Application/DTOs/Status/StatusInsertDTO.cs
@@ -1,4 +1,11 @@
 namespace ClinAgenda.Application.DTOs
+
+/*
+ * Study Note
+ *
+ * When an attribute is marked as "required", it means that it must be initialized before the object is created.
+ */
+
 {
     public class StatusInsertDTO
     {
