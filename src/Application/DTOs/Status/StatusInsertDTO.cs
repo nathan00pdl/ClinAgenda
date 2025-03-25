2f6c2f8fcc58598315db65773c2683402865a49c
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-25 14:21:38 -0300

    study comment refactoring

--- src/Application/DTOs/Status/StatusInsertDTO.cs
+++ src/Application/DTOs/Status/StatusInsertDTO.cs
@@ -1,11 +1,4 @@
 namespace ClinAgenda.Application.DTOs
-
-/*
- * Study Note
- *
- * When an attribute is marked as "required", it means that it must be initialized before the object is created.
- */
-
 {
     public class StatusInsertDTO
     {
--- src/Core/Entities/Status.cs
+++ src/Core/Entities/Status.cs
@@ -1,4 +1,11 @@
 namespace ClinAgenda.Core.Entities
+
+/*
+ * Study Note
+ *
+ * When an attribute is marked as "required", it means that it must be initialized before the object is created.
+ */
+
 {
     public class Status
     {
