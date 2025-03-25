c76bdbb692327a8cf0b9b95b0eb77202231ecdc2
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-25 14:13:21 -0300

    status entity attribute correction

--- src/Core/Entities/Status.cs
+++ src/Core/Entities/Status.cs
@@ -3,6 +3,6 @@ namespace ClinAgenda.Core.Entities
     public class Status
     {
         public int Id { get; set; }
-        public required String name { get; set; }
+        public required String Name { get; set; }
     }
 }
