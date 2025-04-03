48882f386520bf81e958935f2a49d03fab44e90d
nathan00pdl <nathanpaiva00@gmail.com>
2025-04-03 18:58:42 -0300

    add cors handling and database connection configuration

--- clinagenda-backend/Program.cs
+++ clinagenda-backend/Program.cs
@@ -2,14 +2,39 @@ using ClinAgenda.Application.UseCases;
 using ClinAgenda.Core.Interfaces;
 using ClinAgenda.Infrastructure.Repositories;
 using MySql.Data.MySqlClient;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() 
+              .AllowAnyHeader() 
+              .AllowAnyMethod(); 
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+if (connectionString != null)
+{
+    connectionString = connectionString
+        .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+        .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+}
+else
+{
+    throw new InvalidOperationException("SQL connection string is not configured.");
+}
+
 builder.Services.AddScoped<MySqlConnection>(_ => new MySqlConnection(connectionString));
 
 builder.Services.AddScoped<IStatusRepository, StatusRepository>();
@@ -29,7 +54,6 @@ builder.Services.AddScoped<IDoctorSpecialtyRepository, DoctorSpecialtyRepository
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
 builder.Services.AddScoped<AppointmentUseCase>();
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -38,7 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
 
