7bb1c973447772bfea4094328c24a6a7606ce86c
nathan00pdl <nathanpaiva00@gmail.com>
2025-04-03 21:22:54 -0300

    change to inject connection string into Program.cs

--- clinagenda-backend/Program.cs
+++ clinagenda-backend/Program.cs
@@ -1,7 +1,6 @@
 using ClinAgenda.Application.UseCases;
 using ClinAgenda.Core.Interfaces;
 using ClinAgenda.Infrastructure.Repositories;
-using MySql.Data.MySqlClient;
 using DotNetEnv;
 
 Env.Load();
@@ -35,7 +34,7 @@ else
     throw new InvalidOperationException("SQL connection string is not configured.");
 }
 
-builder.Services.AddScoped<MySqlConnection>(_ => new MySqlConnection(connectionString));
+builder.Services.AddSingleton(connectionString);
 
 builder.Services.AddScoped<IStatusRepository, StatusRepository>();
 builder.Services.AddScoped<StatusUseCase>();
