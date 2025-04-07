7f783b836780ec1b5238cc9cb01276399da14abc
nathan00pdl <nathanpaiva00@gmail.com>
2025-04-07 20:40:39 -0300

    treatment of required attribute in StatusInsertDTO

--- clinagenda-backend/src/Application/DTOs/Status/StatusInsertDTO.cs
+++ clinagenda-backend/src/Application/DTOs/Status/StatusInsertDTO.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ClinAgenda.Application.DTOs
 {
     public class StatusInsertDTO
     {
+        [Required(ErrorMessage = "The Status's Name is Mandatory", AllowEmptyStrings = false)]
         public required string Name { get; set; }
     }
 }
